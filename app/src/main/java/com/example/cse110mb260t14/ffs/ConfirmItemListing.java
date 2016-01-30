f69ae50240c7b3037e12d147c6ce423e81dcf9de
gabmech <gmechali@ucsd.edu>
2016-01-30 13:36:39 -0800

    prepare for merge

--- app/src/main/java/com/example/cse110mb260t14/ffs/ConfirmItemListing.java
+++ app/src/main/java/com/example/cse110mb260t14/ffs/ConfirmItemListing.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
--- app/src/main/java/com/example/cse110mb260t14/ffs/DrawerMenuActivity.java
+++ app/src/main/java/com/example/cse110mb260t14/ffs/DrawerMenuActivity.java
@@ -1,17 +1,23 @@
 package com.example.cse110mb260t14.ffs;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,19 +26,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.login.widget.LoginButton;
-import com.parse.LogInCallback;
-import com.parse.ParseException;
-import com.parse.ParseFacebookUtils;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import android.content.Intent;
+import java.util.Locale;
 
 public class DrawerMenuActivity extends ActionBarActivity {
-
-
 
     private ListView mDrawerList;
     private DrawerLayout mDrawerLayout;
@@ -41,6 +43,11 @@ public class DrawerMenuActivity extends ActionBarActivity {
     private String mActivityTitle;
     private LoginButton login_button;
     private Button postButton;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private double latitude, longitude;
+    private Geocoder geocoder;
+    private List<Address> addresses;
 
 
     TextView usernameTextView;
@@ -49,7 +56,6 @@ public class DrawerMenuActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
         mDrawerList = (ListView) findViewById(R.id.navList);
@@ -67,6 +73,19 @@ public class DrawerMenuActivity extends ActionBarActivity {
         usernameTextView = (TextView) findViewById(R.id.usernameTextView);
         usernameTextView.setText(message);
 
+        TextView name_view = (TextView) findViewById(R.id.name_view);
+        TextView location_view = (TextView) findViewById(R.id.location_view);
+
+        if (ParseUser.getCurrentUser().getString("name") != null) {
+            name_view.setText(ParseUser.getCurrentUser().getString("name"));
+        }
+
+        geocoder = new Geocoder(this, Locale.getDefault());
+        // get location
+        startGrabbingLocation(locationManager, locationListener);
+
+        location_view.setText(ParseUser.getCurrentUser().getString("address") + ", " + ParseUser.getCurrentUser().getString("city"));
+
         final List<String> permissions = Arrays.asList("public_profile", "email");
 
         login_button = (LoginButton) findViewById(R.id.login_button);
@@ -79,12 +98,23 @@ public class DrawerMenuActivity extends ActionBarActivity {
             }
         });
 
-        postButton = (Button)findViewById(R.id.sell_button);
+        postButton = (Button) findViewById(R.id.sell_button);
         postButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DrawerMenuActivity.this, PostItemActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        // setup onclicklistener to update location
+        Button location_button = (Button) findViewById(R.id.location_button);
+        location_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateLocation(ParseUser.getCurrentUser());
+                ((TextView)findViewById(R.id.location_view)).setText(ParseUser.getCurrentUser().getString("address")
+                        + ", " + ParseUser.getCurrentUser().getString("city"));
             }
         });
 
@@ -182,6 +212,82 @@ public class DrawerMenuActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void startGrabbingLocation(LocationManager locationManager, LocationListener locationListener) {
+        // start the location manager for retrieving GPS coordinates
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                ParseUser user = ParseUser.getCurrentUser();
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+                try {
+                    addresses = geocoder.getFromLocation(latitude,longitude, 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                updateLocation(user);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        // Check permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        try {
+            Toast.makeText(DrawerMenuActivity.this, "Getting Location From Network Provider", Toast.LENGTH_SHORT).show();
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+        }
+        catch (IllegalArgumentException e) {
+        }
+    }
+    public void updateLocation(ParseUser user) {
+        // Update ParseUser
+                if (user != null) {
+                    user.put("location", new ParseGeoPoint(latitude, longitude));
+
+                    if (addresses.get(0) != null) {
+                        if (addresses.get(0).getAddressLine(0) != null) {
+                            user.put("address", addresses.get(0).getAddressLine(0));
+                        }
+                        if (addresses.get(0).getSubLocality() != null) {
+                            user.put("city", addresses.get(0).getSubLocality());
+                        }
+                        if (addresses.get(0).getAdminArea() != null) {
+                            user.put("state", addresses.get(0).getAdminArea());
+                        }
+                        if (addresses.get(0).getCountryName() != null) {
+                            user.put("country", addresses.get(0).getCountryName());
+                        }
+                        if (addresses.get(0).getPostalCode() != null) {
+                            user.put("postalCode", addresses.get(0).getPostalCode());
+                        }
+                        // save the data to database
+                        user.saveInBackground();
+                    }
+                }
+    }
 }
-
-
