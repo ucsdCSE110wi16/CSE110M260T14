f52ef1ea51c8408a3c16e9d4a4de618a3c4741f9
Deqian Kong <dekong@ucsd.edu>
2016-02-17 14:40:51 -0800

    Adding search bar for listing and modify the listing activity

--- app/src/main/java/com/example/cse110mb260t14/ffs/ListingsActivity.java
+++ app/src/main/java/com/example/cse110mb260t14/ffs/ListingsActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.parse.FindCallback;
@@ -19,16 +21,103 @@ import java.util.List;
  * Created by George on 2/6/2016.
  */
 public class ListingsActivity extends AppCompatActivity {
+        private Button search_button;
+        private ParseQuery<ParseObject> query;
+        String description;
+        String[] Split_description;
         public static ArrayList<String> categories = new ArrayList<String>();
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_listings);
 
 
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("Listings");
+                query = ParseQuery.getQuery("Listings");
                 query.whereContainsAll("Categories", categories);
                 query.whereContains("Title", "");
                 query.whereContains("objectId", "");
+                search_button = (Button) findViewById(R.id.SearchList);
+                search_button.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View a) {
+
+
+                                EditText descriptionText = (EditText) findViewById(R.id.EditTextIdList);
+                                description = descriptionText.getText().toString();
+                                int sub_postion = 0;
+                                for (int i = 0; i < description.length(); i++) {
+                                        if (description.charAt(i) == ' ') {
+                                                sub_postion++;
+                                        } else {
+                                                break;
+                                        }
+                                }
+                                description = description.substring(sub_postion);
+                                System.out.println("What is :" + description);
+                                Split_description = description.split("\\s+");
+                                List<ParseQuery<ParseObject>> queries = new ArrayList<ParseQuery<ParseObject>>();
+                                int word_limit = 0;
+                                if (Split_description.length <= 5) {
+                                        word_limit = Split_description.length;
+                                } else {
+                                        word_limit = 5;
+                                }
+                                for (int i = 0; i < word_limit; i++) {
+                                        ParseQuery<ParseObject> title = ParseQuery.getQuery("Listings");
+
+
+                                        ParseQuery<ParseObject> description = ParseQuery.getQuery("Listings");
+
+
+                                        String lowerCase_description = new String(Split_description[i].toLowerCase());
+
+                                        //System.out.println("SOME:" + Split_description[i]);
+                                        title.whereContains("objectId", "");
+                                        title.whereContains("Title", lowerCase_description);
+
+                                        description.whereContains("objectId", "");
+                                        description.whereContains("Description", lowerCase_description);
+
+                                        // System.out.println("Description is : " + Split_description[i]);
+
+                                        queries.add(title);
+
+                                        queries.add(description);
+
+                                }
+
+                                System.out.println("Finish Finding");
+                                query = ParseQuery.or(queries);
+                                query.whereContainsAll("Categories", categories);
+                                query.findInBackground(new FindCallback<ParseObject>() {
+                                        public void done(List<ParseObject> found, ParseException e) {
+
+                                                ArrayList<ParseObject> objects = new ArrayList<ParseObject>(found);
+                                                ListingAdapter adapter = new ListingAdapter(ListingsActivity.this, objects);
+                                                ListView listView = (ListView) findViewById(R.id.listings);
+                                                listView.setAdapter(adapter);
+
+
+                                                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                                        @Override
+                                                        public void onItemClick(AdapterView<?> adapter, View v, int position, long id) {
+                                                                // do later
+
+                                                                System.out.println("CLICKED ON ITEM  " + ((ParseObject) adapter.getItemAtPosition(position)).getString("Title"));
+
+                                                                Intent intent = new Intent(ListingsActivity.this, displayFullItem.class);
+                                                                intent.putExtra("objectID", ((ParseObject) adapter.getItemAtPosition(position)).getObjectId());
+                                                                System.out.println("LISTING ID IS " + ((ParseObject) adapter.getItemAtPosition(position)).getObjectId());
+                                                                startActivity(intent);
+
+                                                        }
+                                                });
+                                        }
+                                });
+
+                        }
+                });
+
                 query.findInBackground(new FindCallback<ParseObject>() {
                         public void done(List<ParseObject> found, ParseException e) {
                                 /*
