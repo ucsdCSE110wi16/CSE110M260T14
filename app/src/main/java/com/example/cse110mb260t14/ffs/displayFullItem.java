950c3ed5d092b8e91ef900bb8dc78e804d829c38
gabmech <gmechali@ucsd.edu>
2016-02-28 22:48:38 -0800

    Added ability to make offers, and delete your own listings

--- app/src/main/java/com/example/cse110mb260t14/ffs/displayFullItem.java
+++ app/src/main/java/com/example/cse110mb260t14/ffs/displayFullItem.java
@@ -1,5 +1,7 @@
 package com.example.cse110mb260t14.ffs;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,7 +14,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseGeoPoint;
@@ -28,8 +29,8 @@ import java.util.Locale;
 public class displayFullItem extends AppCompatActivity {
 
     private String objectId;
-    private TextView TitleTV, PriceTV, DescriptionTV, CategoriesTV, LocationTV;
-    private Button watchListButton, makeOfferButton;
+    private TextView TitleTV, PriceTV, DescriptionTV, CategoriesTV, LocationTV, OfferTV, DeleteItemTV;
+    private Button watchListButton, makeOfferButton, OfferSubmitButton;
     private boolean WatchListAdd;
     private ImageView photo_display;
     private ParseFile photoByteArray;
@@ -37,6 +38,7 @@ public class displayFullItem extends AppCompatActivity {
     ParseUser user = ParseUser.getCurrentUser();
     ArrayList<String> watchList = (ArrayList)user.get("WatchList");
     private boolean fullscreen = false;
+
 
     ParseObject listing;
 
@@ -56,6 +58,7 @@ public class displayFullItem extends AppCompatActivity {
         photo_display = (ImageView)findViewById(R.id.photo_display);
         watchListButton = (Button)findViewById(R.id.AddToWatchListButton);
         makeOfferButton = (Button)findViewById(R.id.makeOfferButton);
+        OfferSubmitButton = (Button)findViewById(R.id.offer_submit_button);
 
         if(watchList != null && watchList.contains(objectId)){
             watchListButton.setText("Remove from WatchList");
@@ -69,6 +72,30 @@ public class displayFullItem extends AppCompatActivity {
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Listings");
         query.whereContains("objectId", objectId);
+
+        try {
+            List<ParseObject> found = query.find();
+            listing = found.get(0);
+            TitleTV.setText((String) listing.get("Title"));
+            DescriptionTV.setText((String) listing.get("Description"));
+            PriceTV.setText("$" + (String) listing.get("Price"));
+            CategoriesTV.setText(Arrays.asList(listing.get("Categories")).get(0).toString());
+            LocationTV.setText("Pickup Address at: " + ParseUser.getCurrentUser().getString("address")
+                    + ", " + ParseUser.getCurrentUser().getString("city"));
+
+            photoByteArray = (ParseFile) listing.get("photo_byte_array");
+            if (photoByteArray != null) {
+                try {
+                    photoBitmap = BitmapFactory.decodeByteArray(photoByteArray.getData(), 0, photoByteArray.getData().length);
+                    photo_display.setImageBitmap(photoBitmap);
+
+                } catch (ParseException pe) {
+                    Log.v(displayFullItem.this.toString(), "error decoding byte array");
+                }
+            }
+        }catch (ParseException e){};
+
+        /*
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> found, ParseException e) {
                 listing = found.get(0);
@@ -91,8 +118,37 @@ public class displayFullItem extends AppCompatActivity {
                 }
             }
 
-        });
+        });*/
 
+
+        DeleteItemTV = (TextView) findViewById(R.id.delete_item_text_view);
+        if(listing != null && listing.getString("SellerID").equals(ParseUser.getCurrentUser().getObjectId())){
+            DeleteItemTV.setVisibility(View.VISIBLE);
+        }
+
+        DeleteItemTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(displayFullItem.this);
+                builder.setTitle("Are you sure you want to delete this item?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        listing.put("Status", 2);
+                        listing.saveInBackground();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+
+            }
+        });
+        /*
          //TODO: added enlarge image capability
         photo_display.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -124,7 +180,7 @@ public class displayFullItem extends AppCompatActivity {
                     fullscreen = false;
                 }
             }
-        });
+        }); */
 
         LocationTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -151,8 +207,7 @@ public class displayFullItem extends AppCompatActivity {
 
                     watchListButton.setText("Remove from WatchList");
                     WatchListAdd = false;
-                }
-                else {
+                } else {
                     watchList.remove(objectId);
                     watchListButton.setText("Add to WatchList");
                     WatchListAdd = true;
@@ -162,32 +217,67 @@ public class displayFullItem extends AppCompatActivity {
             }
         });
 
-
         makeOfferButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                OfferTV = (TextView) findViewById(R.id.offerTextView);
+                OfferTV.setVisibility(View.VISIBLE);
+                OfferSubmitButton.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
+        OfferSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
                 ArrayList<String> offers_id = (ArrayList<String>) (listing.get("offer_buyer_id"));
-                ArrayList<Integer> offer_value = (ArrayList<Integer>) (listing.get("offer_value"));
+                ArrayList<String> offer_value = (ArrayList<String>) (listing.get("offer_value"));
                 ArrayList<String> user_offers_id = (ArrayList<String>) (user.get("offer_made_id"));
-                ArrayList<Integer> user_offers_value = (ArrayList<Integer>) (user.get("offer_made_value"));
+                ArrayList<String> user_offers_value = (ArrayList<String>) (user.get("offer_made_value"));
 
 
-                if (listing!= null && offers_id != null && user_offers_id!= null && (offers_id.contains(user.getObjectId()) || user_offers_id.contains(listing.getObjectId()))) {
-
+                if (listing == null) {
                 } else {
 
-
+                    if (offers_id == null) {
+                        offers_id = new ArrayList<String>();
+                    }
+                    if (user_offers_id == null) {
+                        user_offers_id = new ArrayList<String>();
+                    }
+                    if (offer_value == null) {
+                        offer_value = new ArrayList<String>();
+                    }
+                    if (user_offers_value == null) {
+                        user_offers_value = new ArrayList<String>();
+                    }
+                    String offer = OfferTV.getText().toString();
                     offers_id.add(user.getObjectId());
-                    offer_value.add(1);
+                    offer_value.add(offer);
                     listing.put("offer_buyer_id", offers_id);
                     listing.put("offer_value", offer_value);
                     listing.saveInBackground();
                     user_offers_id.add(listing.getObjectId());
-                    user_offers_value.add(1);
+                    user_offers_value.add(offer);
                     user.put("offer_made_id", user_offers_id);
                     user.put("offer_made_value", user_offers_value);
                     user.saveInBackground();
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(displayFullItem.this);
+                    builder.setTitle("You offered $" + offer + " for this item!");
+                    builder.setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+
+                    builder.show();
                 }
+                OfferTV.setVisibility(View.GONE);
+                OfferSubmitButton.setVisibility(View.GONE);
             }
         });
 
